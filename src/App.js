69c373966f34a8e7511ef5c505a0b298dcd411ff
Murshedul <murshedbinamir@gmail.com>
2022-04-26 02:10:37 +0600

    my first practice project react with Node ready

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [users, setUsers] = useState([]);
+
+  useEffect( () => {
+    fetch('http://localhost:5000/users')
+    .then(res => res.json())
+    .then(data => setUsers(data))
+  }, [])
+
+  const handleAddUser = (e) => {
+    e.preventDefault();
+
+    const name = e.target.name.value;
+    const email = e.target.email.value;
+
+    const user = {name, email};
+
+    // check user already exist
+    users.forEach(user => {
+      if (email === user.email) {
+        alert('User already exist')
+        return;
+      }
+    })
+
+    // post data to server
+    fetch('http://localhost:5000/user', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(user)
+    })
+    .then(res => res.json())
+    .then(data => {
+      const newUsers = [...users, data];
+      setUsers(newUsers);
+      console.log('succeed to post', data)
+    })
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Our users</h2>
+      <form onSubmit={handleAddUser}>
+        <input type="text" name="name" id="name" placeholder='Your name' required />
+        <input type="email" name="email" id="email" placeholder='Your email' required />
+        <input type="submit" value="Add user" />
+      </form>
+      {
+        users.map(user => <p key={user.id}>
+          <span><strong>Id:</strong> {user.id} </span>
+          <span><strong>Name:</strong> {user.name} </span>
+          <span><strong>Email:</strong> {user.email}</span>
+        </p>)
+      }
     </div>
   );
 }
